e8b54e619eb2b1d9dc8c27f82276308fcff8eb1c
David Prizker <David1213141516@gmail.com>
2026-02-15 23:02:03 +0200

    Update README & Define Basic Defenitions for CefRenderHandler

--- README.md
+++ README.md
@@ -6,4 +6,11 @@ different CEF versions for future changes.
 
 ## Setup
 
-1. TBD
+1. Create build of CEF for windows like in the [CEF DOCS](https://chromiumembedded.github.io/cef/tutorial#windows-build-steps) (Make sure to make it dynamic compile runtime (/MD))
+    * You can use [CEF](https://drive.google.com/file/d/1QK6pRAGeS7i27iVSUpdXzJ6Xmlbha9Wy/view?usp=sharing) that I already compiled with everything
+2. Add Include Folder inside CEF to find refernces ($PWD/CEF/include)
+    * The link I provided for the [CEF](https://drive.google.com/file/d/1QK6pRAGeS7i27iVSUpdXzJ6Xmlbha9Wy/view?usp=sharing) already has it
+3. Define QT 5.15.2 Kit with MSVC 2019
+4. Build the project
+5. Put the CEF Binary in the output directory
+6. Run 
--- Sources/CefClientHandler.cpp
+++ Sources/CefClientHandler.cpp
@@ -11,13 +11,33 @@ void CefClientHandler::OnPaint(
 ) {
 
 }
-bool CefClientHandler::GetViewRect(
+void CefClientHandler::GetViewRect(
     CefRefPtr<CefBrowser> browser,
     CefRect& rect
 ) {
 
 }
+
 bool CefClientHandler::GetScreenInfo(CefRefPtr<CefBrowser> browser ,CefScreenInfo& screen_info) {
-    screen_info.Set(0.0, 0, 0, false, nullptr, nullptr);
     return false;
 }
+
+
+CefRefPtr<CefAccessibilityHandler> CefClientHandler::GetAccessibilityHandler(){
+    CefRefPtr<CefAccessibilityHandler> temp;
+    return temp;
+}
+
+bool CefClientHandler::GetRootScreenRect(CefRefPtr<CefBrowser> browser, CefRect& rect) { return false; }
+bool CefClientHandler::GetScreenPoint(CefRefPtr<CefBrowser> browser, int viewX, int viewY, int& screenX, int& screenY) { return false; }
+void CefClientHandler::OnPopupShow(CefRefPtr<CefBrowser> browser, bool show) { }
+void CefClientHandler::OnPopupSize(CefRefPtr<CefBrowser> browser, const CefRect& rect) { }
+void CefClientHandler::OnAcceleratedPaint(CefRefPtr<CefBrowser> browser, PaintElementType type, const RectList& dirtyRects, const CefAcceleratedPaintInfo& info) { }
+void CefClientHandler::GetTouchHandleSize(CefRefPtr<CefBrowser> browser, cef_horizontal_alignment_t orientation, CefSize& size) { }
+void CefClientHandler::OnTouchHandleStateChanged(CefRefPtr<CefBrowser> browser, const CefTouchHandleState& state) { }
+bool CefClientHandler::StartDragging(CefRefPtr<CefBrowser> browser, CefRefPtr<CefDragData> drag_data, DragOperationsMask allowed_ops, int x, int y) { return false; }
+void CefClientHandler::UpdateDragCursor(CefRefPtr<CefBrowser> browser, DragOperation operation) { }
+void CefClientHandler::OnScrollOffsetChanged(CefRefPtr<CefBrowser> browser, double x, double y) { }
+void CefClientHandler::OnImeCompositionRangeChanged(CefRefPtr<CefBrowser> browser, const CefRange& selected_range, const RectList& character_bounds) { }
+void CefClientHandler::OnTextSelectionChanged(CefRefPtr<CefBrowser> browser, const CefString& selected_text, const CefRange& selected_range) { }
+void CefClientHandler::OnVirtualKeyboardRequested(CefRefPtr<CefBrowser> browser, TextInputMode input_mode) { }
